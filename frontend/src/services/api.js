13d9c665aef0444b81336064b459c507cfd0bf7f
miniyarlajili <miniarlajili26@gmail.com>
2026-05-05 21:02:09 +0100

    fix: update API URL for kubernetes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
 });
-
 export const getNotes = () => API.get('/notes');
 export const createNote = (data) => API.post('/notes', data);
 export const updateNote = (id, data) => API.put(`/notes/${id}`, data);
